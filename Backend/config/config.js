bd9cc05ba6631f56f5718e5552e69d8aebe1cf5b
aavaig-malhotra <aavaigmalhotra2001@gmail.com>
2022-08-30 18:09:19 +0530

    add : backend & razorpay

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -1,10 +1,15 @@
-var mongo = require("mongoose");  
+var mongo = require("mongoose");
 
-var db =   
-mongo.connect("mongodb://localhost:27017/flights", { useNewUrlParser: true, useUnifiedTopology: true },function(err, response){  
-   if(err){ console.log('Failed to connect to ' + db); }  
-   else{ console.log('Connected to akshat ' + db, ' + ', response); }  
-});  
+var db = mongo.connect(
+  "mongodb+srv://quickai:quickai@cluster0.fygoyj1.mongodb.net/flights?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function (err, response) {
+    if (err) {
+      console.log("Failed to connect to " + db);
+    } else {
+      console.log("Connected to akshat " + db, " + ", response);
+    }
+  }
+);
 
-
-module.exports =db;  
+module.exports = db;
--- Backend/keys.js
+++ Backend/keys.js
@@ -1,4 +1,4 @@
-module.exports={
-    razorIdkey : 'rzp_live_kkicnBJoqVlLOQ',
-    razorIdSecret: 'Ef8AOa9g6Bwa6HoqWcWuBXkq'
-}
+module.exports = {
+  razorIdkey: "rzp_test_8TEr0yyWpFNHN6",
+  razorIdSecret: "jAUhJwHtlmQ85iQKJyhADjeG",
+};
